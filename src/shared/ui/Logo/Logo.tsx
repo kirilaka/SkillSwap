398e25f5c9ada83b1fc2e0631f2f0c1df9207869
Maxim Kondratiev <kondrati3vmaksim@gmail.com>
2026-06-12 02:19:00 +0300

    logo component adde text

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { FC } from 'react'
 import logoSvg from '@/assets/icons/logo.svg'
-import styles from './Logo.module.css'
+import styles from './Logo.module.scss'
 
 interface LogoProps {
   className?: string
@@ -11,6 +11,7 @@ export const Logo: FC<LogoProps> = ({ className }) => {
   return (
     <Link to="/" className={`${className}`} aria-label="SkillSwap - на главную">
       <img src={logoSvg} alt="SkillSwap логотип" className={styles.icon} />
+      <span className={styles.text}>SkillSwap</span>
     </Link>
   )
 }
