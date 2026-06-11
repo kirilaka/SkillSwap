cf2e2dca426df16465103e773a13fd0041fe7dad
Groulbands <groulbands@gmail.com>
2026-06-11 14:43:21 +0300

    feat(packaje.json): add additional config

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/app/styles/variables" as *;`,
+        additionalData: `@use "src/styles/variables.scss" as *;`,
       },
     },
   },
