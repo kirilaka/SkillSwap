62f9fa8b1c0b3ea883250cb8530fc311c0caf06f
Maxim Kondratiev <kondrati3vmaksim@gmail.com>
2026-06-12 02:03:02 +0300

    logo component

---
+++ src/shared/ui/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import { FC } from 'react'
+import logoSvg from '@/assets/icons/logo.svg'
+import styles from './Logo.module.css'
+
+interface LogoProps {
+  className?: string
+}
+
+export const Logo: FC<LogoProps> = ({ className }) => {
+  return (
+    <Link to="/" className={`${className}`} aria-label="SkillSwap - на главную">
+      <img src={logoSvg} alt="SkillSwap логотип" className={styles.icon} />
+    </Link>
+  )
+}
