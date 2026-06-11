46a8052afb295fefd701773f88868d5ed7c72428
ovladsmit <qazwsxedcqazwsxedc820@gmail.com>
2026-06-11 22:34:13 +0300

    feat: добавлен компонент ArrowIcon

---
+++ src/shared/ui/ArrowIcon/ArrowIcon.tsx
@@ -0,0 +1,16 @@
+import styles from './ArrowIcon.module.css'
+import ArrowSvg from './Arrow.svg?react'
+
+type Orientation = 'vertical' | 'horizontal'
+
+type ArrowIconPrors = {
+  isOpen?: boolean;
+  orientation?: Orientation;
+  className? :string;
+}
+
+export const ArrowIcon = ({isOpen = false, orientation = 'horizontal', className}: ArrowIconPrors) => {
+  return(
+    <ArrowSvg className={`${styles.arrow} ${styles[orientation]} ${isOpen ? styles.open: ''} ${className}`}/>
+  )
+}
