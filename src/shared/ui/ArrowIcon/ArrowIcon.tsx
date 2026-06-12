ef4d7dde7694b475dc5fe33366e946501730f4f6
Groulbands <groulbands@gmail.com>
2026-06-12 22:23:25 +0300

    refactor(ui/arrowicon): refactor clsx, add desc
    add desc. for props

--- src/shared/ui/ArrowIcon/ArrowIcon.tsx
+++ src/shared/ui/ArrowIcon/ArrowIcon.tsx
@@ -1,16 +1,31 @@
 import styles from './ArrowIcon.module.scss'
 import ArrowSvg from './Arrow.svg?react'
+import clsx from 'clsx'
 
 type Orientation = 'vertical' | 'horizontal'
 
-type ArrowIconPrors = {
-  isOpen?: boolean;
-  orientation?: Orientation;
-  className? :string;
+interface ArrowIconPrors {
+  /** Состояние открыта/закрыта */
+  isOpen?: boolean
+  /** Ориентация, по умол. horizontal*/
+  orientation?: Orientation
+  /** Доп. классы */
+  className?: string
 }
 
-export const ArrowIcon = ({isOpen = false, orientation = 'horizontal', className}: ArrowIconPrors) => {
-  return(
-    <ArrowSvg className={`${styles.arrow} ${styles[orientation]} ${isOpen ? styles.open: ''} ${className}`}/>
+export const ArrowIcon = ({
+  isOpen = false,
+  orientation = 'horizontal',
+  className,
+}: ArrowIconPrors) => {
+  return (
+    <ArrowSvg
+      className={clsx(
+        styles.arrow,
+        styles[orientation],
+        isOpen && styles[`${orientation}_open`],
+        className,
+      )}
+    />
   )
 }
