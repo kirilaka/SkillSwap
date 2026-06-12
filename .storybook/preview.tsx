39c759655dd3b1bf4ae384019cda8b971ffa4dbd
Evelina Ayrapetova <evelina.airapetova97@yandex.ru>
2026-06-12 18:58:54 +0300

    docs(storybook): базовая настройка storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react-vite'
+import '../src/app/styles/global.scss'
+
+const preview: Preview = {
+  tags: ['autodocs'],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
