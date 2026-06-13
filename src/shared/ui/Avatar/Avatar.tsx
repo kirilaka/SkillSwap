5e28d4b68e4d684e9e9b834a1a24a537df027bb2
Groulbands <groulbands@gmail.com>
2026-06-13 23:58:05 +0300

    refactor(avatar): change props necessity, rename classname
    [FEATURE] Компонент Avatar #24

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -2,22 +2,11 @@ import styles from './Avatar.module.scss';
 import clsx from 'clsx';
 import fallbackSvg from './fallback.svg';
 
-/**
- * Avatar — компонент отображения пользовательского изображения с fallback-заглушкой.
- *
- * Если `src` не передан, отображается fallback-изображение.
- *
- * @param {string} src — URL основного изображения.
- * @param {string} alt — Атрибут alt для доступности.
- * @param {string} [fallbackSrc] — Путь к fallback-изображению.
- * @param {string} [className] — Дополнительные CSS-классы.
- */
-
 interface AvatarProps {
   /** URL основного изображения */
-  src: string;
+  src?: string;
   /** Атрибут alt для доступности */
-  alt: string;
+  alt?: string;
   /** Путь к fallback-изображению */
   fallbackSrc?: string;
   /** Дополнительные CSS-классы */
@@ -30,7 +19,7 @@ export const Avatar = ({ src, alt, fallbackSrc = fallbackSvg, className }: Avata
 
   return (
     <div className={clsx(styles.avatar, className)}>
-      <img src={imageSrc} alt={alt} />
+      <img src={imageSrc} alt={alt} className={styles.avatarImage} />
     </div>
   );
 };
