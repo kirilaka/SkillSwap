bc2f2d7c7c0dcb7ff206d2cb9d221d0329db3d38
Maxim Kondratiev <kondrati3vmaksim@gmail.com>
2026-06-15 16:05:37 +0300

    feat(add-useravatar/userstories/usermodule): добавлен аватар

---
+++ src/entities/ui/UserAvatar/UserAvatar.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { UserAvatar } from './UserAvatar';
+import { fn } from 'storybook/test';
+const meta: Meta<typeof UserAvatar> = {
+  title: 'Entities/UserAvatar/UserAvatar',
+  component: UserAvatar,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    onClick: fn(),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UserAvatar>;
+
+const mockUser = {
+  id: '1',
+  name: 'Иван Петров',
+  email: 'ivan@example.com',
+  avatarUrl: 'https://placehold.co/100',
+  createdAt: '2026-01-01',
+  city: 'Москва',
+  age: 31,
+};
+
+export const NameFormat: Story = {
+  name: 'Формат: только имя',
+  args: {
+    user: mockUser,
+    infoFormat: 'name',
+  },
+};
+
+export const AllFormat: Story = {
+  name: 'Формат: вся информация',
+  args: {
+    user: mockUser,
+    infoFormat: 'all',
+  },
+};
---
+++ src/entities/ui/UserAvatar/UserAvatar.tsx
@@ -0,0 +1,48 @@
+import { User } from '@/shared/types';
+import styles from './UserAvatar.module.scss';
+import clsx from 'clsx';
+import { Avatar } from '@/shared/ui/Avatar/Avatar';
+
+interface UserInfo extends User {
+  city?: string;
+  age?: number;
+}
+
+interface UserAvatarProps {
+  user: UserInfo;
+  infoFormat: 'name' | 'all';
+  onClick: () => void;
+  className?: string;
+}
+
+const getAgeLabel = (age: number): string => {
+  const lastDigit = age % 10;
+  const lastTwoDigits = age % 100;
+
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 19) return 'лет';
+  if (lastDigit === 1) return 'год';
+  if (lastDigit >= 2 && lastDigit <= 4) return 'года';
+  return 'лет';
+};
+
+export const UserAvatar = ({ user, infoFormat, onClick, className }: UserAvatarProps) => {
+  return (
+    <div className={clsx(styles.userAvatar, styles[infoFormat], className)} onClick={onClick}>
+      <Avatar
+        className={infoFormat === 'all' ? styles.avatarLarge : styles.avatarSmall}
+        src={user.avatarUrl ?? undefined}
+        alt={user.name}
+      />
+
+      <div className={styles.info}>
+        <span className={styles.nameText}>{user.name}</span>
+        {infoFormat === 'all' && user.city && (
+          <span className={styles.details}>
+            {user.city}
+            {user.age ? `, ${user.age} ${getAgeLabel(user.age)}` : ''}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
