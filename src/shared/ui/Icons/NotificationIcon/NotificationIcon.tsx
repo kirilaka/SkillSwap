6c8422aff181f4fa0853120f94cec4a4c96ed111
Elena <lenchik.beautiful@gmail.com>
2026-06-17 12:05:45 +0300

    feat: NotificationIcon с точкой 12x12

--- src/shared/ui/Icons/NotificationIcon/NotificationIcon.tsx
+++ src/shared/ui/Icons/NotificationIcon/NotificationIcon.tsx
@@ -1,15 +1,21 @@
 import { IconWrapper } from '@/shared/ui/Icons/IconWrapper';
 import notificationSvg from './NotificationIcon.svg';
+import styles from './NotificationIcon.module.scss';
 
 interface NotificationIconProps {
   /** Дополнительные CSS-классы */
   className?: string;
+  /** Наличие новых уведомлений */
+  hasNew?: boolean;
 }
 
-export const NotificationIcon = ({ className }: NotificationIconProps) => {
+export const NotificationIcon = ({ className, hasNew = false }: NotificationIconProps) => {
   return (
-    <IconWrapper className={className}>
-      <img src={notificationSvg} alt="" />
-    </IconWrapper>
+    <div className={`${styles.wrapper} ${className || ''}`}>
+      <IconWrapper>
+        <img src={notificationSvg} alt="" />
+      </IconWrapper>
+      {hasNew && <span className={styles.dot} />}
+    </div>
   );
 };
