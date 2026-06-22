d7bf9a4cca419aee9d61de65961879f36110b0d5
Дмитрий <165948697+D1midron@users.noreply.github.com>
2026-06-22 12:05:14 +0300

    feat(header): добавлен компонент  SearchInput и  исправлено позиционирование  базового инпута

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, forwardRef, InputHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import styles from './Input.module.scss';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   /** Обработчик изменения значения в инпуте */
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   /**
