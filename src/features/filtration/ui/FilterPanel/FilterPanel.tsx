f1aaaddd6a9c246d52bcbe66b258f2b3f4f90a11
Ilya Sokolov <iluwa8@yandex.ru>
2026-06-24 13:08:45 +0300

    feat(ui: add filter panel): uI: Add filter panel

--- src/features/filtration/ui/FilterPanel/FilterPanel.tsx
+++ src/features/filtration/ui/FilterPanel/FilterPanel.tsx
@@ -11,7 +11,7 @@ import {
   updateSkillsWithCallback,
   updateExchangeTypeWithCallback,
   updateGenderWithCallback,
-} from '../../models/FIltrationUtils';
+} from '@/features/filtration/models/FiltrationUtils';
 import styles from './FilterPanel.module.scss';
 
 export interface FilterPanelProps {
