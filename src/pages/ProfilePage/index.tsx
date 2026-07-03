d4625339411e4ffb4cefe9f1df815b702e694190
Groulbands <groulbands@gmail.com>
2026-07-03 11:30:42 +0300

    feat: добавлена обработка текущего пользователя

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,11 +1,11 @@
 import styles from './Profile.module.scss';
 import { ProfileSidebar } from '@/widgets/Profile/ui/ProfileSidebar/ProfileSidebar';
 import { ProfileContent } from '@/widgets/Profile/ui/ProfileContent/ProfileContent';
-import { getUser } from '@/entities/user/model/userSelectors';
+import { selectAuthUser } from '@/features/auth/model/authSlice';
+import { useAppSelector } from '@/store/hooks';
 
 export default function ProfilePage() {
-  const user = getUser();
-
+  const user = useAppSelector(selectAuthUser);
   return (
     <main className={styles.container}>
       <ProfileSidebar className={styles.sidebar} />
